928531defc7885a6dc99bb09067e7db1191b55f4
agotofio <fasterff2021@gmail.com>
2025-12-28 03:12:20 +0200

    Delete backend directory

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-
-import express from 'express';
-const app = express();
-app.use(express.json());
-app.post('/api/booking',(req,res)=>{
-  console.log('NEW BOOKING',req.body);
-  res.json({ok:true});
-});
-app.listen(3001,()=>console.log('Backend ready'));

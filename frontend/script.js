038de859e937cfca95475ab6f6808d770e371bac
agotofio <fasterff2021@gmail.com>
2025-12-28 03:12:54 +0200

    Delete frontend directory

--- frontend/script.js
+++
@@ -1,9 +0,0 @@
-
-const popup=document.getElementById('popup');
-const payment=document.getElementById('paymentPopup');
-const success=document.getElementById('successPopup');
-document.getElementById('openBooking').onclick=()=>popup.style.display='flex';
-document.getElementById('openBookingHero').onclick=()=>popup.style.display='flex';
-document.getElementById('openPayment').onclick=()=>{popup.style.display='none';payment.style.display='flex';document.getElementById('paymentAmount').innerText='800 грн';};
-document.getElementById('confirmPayment').onclick=()=>{payment.style.display='none';success.style.display='flex';};
-document.getElementById('closeSuccess').onclick=()=>success.style.display='none';
